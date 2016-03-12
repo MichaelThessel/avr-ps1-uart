75516cda2688a5bf5a31c0ae90ce52d80e3496de
Michael Thessel <michael.thessel@gmx.de>
2016-03-12 11:50:46 -1000

    Added sleep mode support

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/sleep.h>
 
 // http://goo.gl/kx5pLU
 #include "dbg_putchar.h"
@@ -19,26 +20,36 @@ int main(void)
 	cli();
 	
 	// Configure PB3 & 4 as inputs
-	DDRB &= ~(1 << DDB3);
-	DDRB &= ~(1 << DDB4);
+	DDRB &= ~((1 << DDB3) | (1 << DDB4));
 	// Enable pin change interrupt
-	GIMSK |= (1 << PCIE);	// Enable interrupts for PB3 & 4	PCMSK |= (1 << PCINT3);	PCMSK |= (1 << PCINT4);		sei();
-	while(1) {
+	GIMSK |= (1 << PCIE);	// Enable interrupts for PB3 & 4	PCMSK |= ((1 << PCINT3) | (1 << PCINT4));	// Enable interrupts	sei();	// Set sleep mode - Power down	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	while(1) {
 		// Button debounce
 		if (pb3_delay > 0) pb3_delay--;
 		if (pb4_delay > 0) pb4_delay--;
+
+		cli();
+		// Enter sleep mode
+		if (pb3_delay == 0 && pb4_delay == 0) {
+		   sleep_enable();
+		   sleep_bod_disable();
+		   sei();
+		   sleep_cpu();
+		   sleep_disable();		}
 	}
-	
+
 	return 0;
 }
 
 ISR(PCINT0_vect) {
-	if(pb3_delay == 0 && PINB & (1 << PB3)) {
+	// Detect pin change to high on PB3
+	if (pb3_delay == 0 && PINB & (1 << PB3)) {
 		pb3_delay = bounce_delay; // Debounce
 		dbg_putchar('[');
     }
 
-	if(pb4_delay == 0 && PINB & (1 << PB4)) {
+	// Detect pin change to high on PB4
+	if (pb4_delay == 0 && PINB & (1 << PB4)) {
 		pb4_delay = bounce_delay; // Debounce
 		dbg_putchar(']');
 	}
