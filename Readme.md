8207713ebee7efee1fa3ddf3d10a8d2716ed23bd
Michael Thessel <michael.thessel@gmx.de>
2016-03-12 10:24:52 -1000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+### PS 1 controller mod - additional button support for EZ-Key ###
+
+This code is for a modified version of (this)[http://imgur.com/a/mb5eN]
+project. The Bluefruit EZ-Key only supports up to 12 inputs. The PS1 controller
+has 14 buttons though. To have all buttons working this project utilizes the
+Serial interface of the EZ-Key. Start & Select will be connected to the
+ATTiny85 pins PB3 & 4. Pin change interrupts on these pins will send ASCII
+chars through the serial interface to the EZ-Key. By combining the EZ-Key and
+the ATTiny85 all buttons on the PS 1 controller are functional.
---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// http://goo.gl/kx5pLU
+#include "dbg_putchar.h"
+
+// Debounce delay
+volatile uint16_t pb3_delay = 0;
+volatile uint16_t pb4_delay = 0;
+uint16_t bounce_delay = 0x0FFF;
+
+int main(void)
+{
+	dbg_tx_init();
+	
+	// Disable interrupts
+	cli();
+	
+	// Configure PB3 & 4 as inputs
+	DDRB &= ~(1 << DDB3);
+	DDRB &= ~(1 << DDB4);
+	// Enable pin change interrupt
+	GIMSK |= (1 << PCIE);	// Enable interrupts for PB3 & 4	PCMSK |= (1 << PCINT3);	PCMSK |= (1 << PCINT4);		sei();
+	while(1) {
+		// Button debounce
+		if (pb3_delay > 0) pb3_delay--;
+		if (pb4_delay > 0) pb4_delay--;
+	}
+	
+	return 0;
+}
+
+ISR(PCINT0_vect) {
+	if(pb3_delay == 0 && PINB & (1 << PB3)) {
+		pb3_delay = bounce_delay; // Debounce
+		dbg_putchar('[');
+    }
+
+	if(pb4_delay == 0 && PINB & (1 << PB4)) {
+		pb4_delay = bounce_delay; // Debounce
+		dbg_putchar(']');
+	}
+}
